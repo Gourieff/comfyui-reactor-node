f3b01a474818f6cf5fbd6d963e92a1728918c28b
Gourieff <777@lovemet.ru>
2023-11-02 13:43:17 +0700

    UPDATE: Retinaface torch dml device support
    +VersionUP (0.3.1 beta2)

--- r_facelib/detection/retinaface/retinaface.py
+++ r_facelib/detection/retinaface/retinaface.py
@@ -16,6 +16,8 @@ if torch.cuda.is_available():
     device = torch.device('cuda')
 elif torch.backends.mps.is_available():
     device = torch.device('mps')
+elif torch.dml.is_available():
+    device = torch.device('dml')
 else:
     device = torch.device('cpu')
 
--- scripts/reactor_version.py
+++ scripts/reactor_version.py
@@ -1,5 +1,5 @@
 app_title = "ReActor Node for ComfyUI"
-version_flag = "v0.3.1-b1"
+version_flag = "v0.3.1-b2"
 
 from scripts.reactor_logger import logger
 
