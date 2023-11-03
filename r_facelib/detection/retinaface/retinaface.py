2bed72a28ed31be68a7097ac64b5233c1fb8c08f
Gourieff <777@lovemet.ru>
2023-11-03 12:39:18 +0700

    HOTFIX2: hasattr correction

--- r_facelib/detection/retinaface/retinaface.py
+++ r_facelib/detection/retinaface/retinaface.py
@@ -16,7 +16,7 @@ if torch.cuda.is_available():
     device = torch.device('cuda')
 elif torch.backends.mps.is_available():
     device = torch.device('mps')
-elif torch.hasattr('dml'):
+elif hasattr(torch,'dml'):
     device = torch.device('dml')
 else:
     device = torch.device('cpu')
