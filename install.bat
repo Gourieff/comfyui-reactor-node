340bb0584cb31ff194dacd23633c679b9410dced
ScrivenerPrime <132724202+ScrivenerPrime@users.noreply.github.com>
2024-01-23 00:37:21 -0600

    check for python in windows path before giving up

--- install.bat
+++ install.bat
@@ -1,15 +1,25 @@
 @echo off
 
-:: Exit if embedded python is not found
-if not exist ..\..\..\python_embeded\python.exe (
-    echo Embedded python not found. Please install manually.
-    pause
-    exit /b 1
+:: Try to use python from the PATH
+python --version >nul 2>&1
+if errorlevel 1 (
+    :: Python not found in PATH, check for embedded python
+    if not exist ..\..\..\python_embeded\python.exe (
+        echo Python not found in PATH and embedded python not found. Please install manually.
+        pause
+        exit /b 1
+    ) else (
+        :: Use the embedded python
+        set PYTHON=..\..\..\python_embeded\python.exe
+    )
+) else (
+    :: Use python from the PATH
+    set PYTHON=python
 )
 
 :: Install the package
 echo Installing...
-..\..\..\python_embeded\python.exe install.py
+%PYTHON% install.py
 echo Done!
 
 @pause
